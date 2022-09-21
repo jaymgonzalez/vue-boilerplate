3626fa126417f72d793118bf2ca0eeeceb004f93
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-21 14:55:06 +0200

    adding quasar

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/global.css'
+import 'quasar/dist/quasar.css'
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,6 @@
 <template>
   <main>
     <h1>Hello World!</h1>
+    <q-btn style="background: #ff0080; color: white" label="Fuchsia" />
   </main>
 </template>
