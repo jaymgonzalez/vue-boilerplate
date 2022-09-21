c524deac27d2d6a7018f931f45dc9cd05437ff64
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-21 21:29:00 +0200

    add dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+const darkQuery = '(prefers-color-scheme: dark)'
+const mediaQuery = window.matchMedia(darkQuery)
+$q.dark.set(mediaQuery.matches)
+mediaQuery.addEventListener('change', (e) => {
+  $q.dark.set(e.matches)
+})
 </script>
 
 <template>
