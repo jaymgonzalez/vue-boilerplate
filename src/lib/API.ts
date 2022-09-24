6eedc1ae5aacf6e5f87760064d0c93191b1e509b
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-24 12:53:00 +0200

    add api

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -3,3 +3,13 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'http://localhost:5000/api/v1',
 })
+
+export async function findAll() {
+  const { data } = await api.get('/todos')
+  return data
+}
+
+export async function createOne() {
+  const { data } = await api.post('/todos')
+  return data
+}
